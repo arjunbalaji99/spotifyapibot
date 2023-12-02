cb0989a2457dc2aa2471ba5f03b31f254ee5d05e
arjunbalaji99 <arjunbalaji08@gmail.com>
2023-12-02 12:54:37 -0800

    im dumb

--- app.py
+++ app.py
@@ -81,9 +81,9 @@ def tallytotals():
         counter = counter - 1
 
 
-if __name__ == '__main__':
-    app.run(threaded=True, port=int(os.environ.get("PORT",
-                                                   os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))
-
 # if __name__ == '__main__':
-#     app.run(threaded=True)
+#     app.run(threaded=True, port=int(os.environ.get("PORT",
+#                                                    os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))
+
+if __name__ == '__main__':
+    app.run(threaded=True)
