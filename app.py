a7a41da0bdb562a1a228b210fd223d0ff2723688
arjunbalaji99 <arjunbalaji08@gmail.com>
2023-12-02 12:54:17 -0800

    added a bunch of stuff

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
-"""
-Prerequisites
-
-    pip3 install spotipy Flask Flask-Session
-
-    // from your [app settings](https://developer.spotify.com/dashboard/applications)
-    export SPOTIPY_CLIENT_ID=client_id_here
-    export SPOTIPY_CLIENT_SECRET=client_secret_here
-    export SPOTIPY_REDIRECT_URI='http://127.0.0.1:8080' // must contain a port
-    // SPOTIPY_REDIRECT_URI must be added to your [app settings](https://developer.spotify.com/dashboard/applications)
-    OPTIONAL
-    // in development environment for debug output
-    export FLASK_ENV=development
-    // so that you can invoke the app outside of the file's directory include
-    export FLASK_APP=/path/to/spotipy/examples/app.py
-
-    // on Windows, use `SET` instead of `export`
-
-Run app.py
-
-    python3 app.py OR python3 -m flask run
-    NOTE: If receiving "port already in use" error, try other ports: 5000, 8090, 8888, etc...
-        (will need to be updated in your Spotify app and SPOTIPY_REDIRECT_URI variable)
-"""
+# remember to export client id, secret and redirect to run in development mode
 
 import os
-from flask import Flask, session, request, redirect
+from flask import Flask, session, request, redirect, render_template
 from flask_session import Session
 import spotipy
 
@@ -34,33 +11,26 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(app)
 
+total_top_artists = {}
+total_top_songs = {}
 
 @app.route('/')
 def index():
-
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
-    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-top-read',
                                                cache_handler=cache_handler,
                                                show_dialog=True)
 
     if request.args.get("code"):
-        # Step 2. Being redirected from Spotify auth page
         auth_manager.get_access_token(request.args.get("code"))
         return redirect('/')
 
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
-        # Step 1. Display sign in link when no token
         auth_url = auth_manager.get_authorize_url()
         return f'<h2><a href="{auth_url}">Sign in</a></h2>'
 
-    # Step 3. Signed in, display data
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
-           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
-           f'<a href="/playlists">my playlists</a> | ' \
-           f'<a href="/currently_playing">currently playing</a> | ' \
-        f'<a href="/current_user">me</a>' \
-
+    return render_template('index.html', name = spotify.me()['display_name'])
 
 
 @app.route('/sign_out')
@@ -68,49 +38,52 @@ def sign_out():
     session.pop("token_info", None)
     return redirect('/')
 
+@app.route('/create_new_group')
+def create_new_group():
+    total_top_artists.clear()
+    total_top_songs.clear()
 
-@app.route('/playlists')
-def playlists():
+    tallytotals()
+    return redirect("/datadisplay")
+
+@app.route('/join_existing_group')
+def join_existing_group():
+    tallytotals()
+    return redirect("/datadisplay")
+
+@app.route('/datadisplay')
+def datadisplay():
+    return render_template('datadisplay.html', totaltopartists = total_top_artists, totaltopsongs = total_top_songs)
+
+def tallytotals():
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         return redirect('/')
 
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return spotify.current_user_playlists()
+    top_artists = spotify.current_user_top_artists(limit=50, offset=0, time_range="medium_term")
+    counter = 50
+    for artist in top_artists['items']:
+        if artist['name'] in total_top_artists:
+            total_top_artists[artist['name']] = total_top_artists[artist['name']] + counter
+        else:
+            total_top_artists[artist['name']] = counter
+        counter = counter - 1
+    
+    top_songs = spotify.current_user_top_artists(limit=50, offset=0, time_range="medium_term")
+    counter = 50
+    for songs in top_songs['items']:
+        if songs['name'] in total_top_songs:
+            total_top_songs[songs['name']] = total_top_songs[songs['name']] + counter
+        else:
+            total_top_songs[songs['name']] = counter
+        counter = counter - 1
 
-
-@app.route('/currently_playing')
-def currently_playing():
-    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
-    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
-    if not auth_manager.validate_token(cache_handler.get_cached_token()):
-        return redirect('/')
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-    track = spotify.current_user_playing_track()
-    if not track is None:
-        return track
-    return "No track currently playing."
-
-
-@app.route('/current_user')
-def current_user():
-    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
-    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
-    if not auth_manager.validate_token(cache_handler.get_cached_token()):
-        return redirect('/')
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return spotify.current_user()
-
-
-'''
-Following lines allow application to be run more conveniently with
-`python app.py` (Make sure you're using python3)
-(Also includes directive to leverage pythons threading capacity.)
-'''
-# if __name__ == '__main__':
-#     app.run(threaded=True, port=int(os.environ.get("PORT",
-#                                                    os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))
 
 if __name__ == '__main__':
-    app.run(threaded=True)
+    app.run(threaded=True, port=int(os.environ.get("PORT",
+                                                   os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))
+
+# if __name__ == '__main__':
+#     app.run(threaded=True)
