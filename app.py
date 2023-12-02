a4c942bce8b1861326c4736671a1a7a8340ed2be
arjunbalaji99 <arjunbalaji08@gmail.com>
2023-12-02 15:30:03 -0800

    i have brain dmg

--- app.py
+++ app.py
@@ -127,9 +127,9 @@ def gettrackinfo():
     return tracks_info
 
 
-if __name__ == '__main__':
-    app.run(threaded=True, port=int(os.environ.get("PORT",
-                                                   os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))
-
 # if __name__ == '__main__':
-#     app.run(threaded=True)
+#     app.run(threaded=True, port=int(os.environ.get("PORT",
+#                                                    os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))
+
+if __name__ == '__main__':
+    app.run(threaded=True)
